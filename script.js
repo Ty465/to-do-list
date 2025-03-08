b4c853af0ebf080de83b085132ce1f661cf01583
Ty465 <tyhamsher16@gmail.com>
2025-03-07 18:27:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+// this is how we grab id elements from html
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+//addTask() is the id for the button
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must add a note!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    /*span is the css for an x */
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    inputBox.click();
+  }
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
